3aaa907d09451c5c9727b85523638f8ad233d685
kissidaniel842 <kissidaniel842@gmail.com>
2025-03-14 10:45:08 -0400

    Done!

--- src/main/java/SkillBuilder4.java
+++ src/main/java/SkillBuilder4.java
@@ -3,16 +3,34 @@
  * SkillBuilder5 is a class for completing the Skill
  * Builder 5 assignment in Java.
  *
- * @author <You>
+ * @author <Daniel Kissi>
  * @version 1.0
  */
 
 public class SkillBuilder4 {
+    static Boolean notT = false;
 
-    // replace this line with your constants
 
     public static String findTYPattern(String s)
     {
-        // replace this line with your code
+        notT = false;
+        String place = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'T' || s.charAt(i) == 't') {
+                notT = true;
+            }
+            if (notT == true) {
+                place += s.charAt(i);
+                if (s.charAt(1) == 'Y' || s.charAt(i) == 'y') {
+                    notT = false;
+                }
+            }
+            if (place.contains("y") || place.contains("Y")) {
+                return place;
+            }
+        }
+
+        return "";
     }
 }
+
